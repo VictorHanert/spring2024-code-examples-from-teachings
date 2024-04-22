639f33d4133c8f0d1a1e0545de24bb4ef8fe6318
Victor <113124594+VictorHanert@users.noreply.github.com>
2024-04-22 06:41:55 +0000

    ses10

--- ses10/bank_with_properties.py
+++ ses10/bank_with_properties.py
@@ -1,45 +1,89 @@
 class Bank:
     def __init__(self, name):
-        self.name = name
-        self.accounts = []
+        self._name = name
+        self._accounts = []
 
     def add_account(self, account):
-        self.accounts.append(account)
-    
-    # lambda function defines the sorting criteria for the list of accounts in the bank.
+        self._accounts.append(account)
+
     def sort_accounts_by_customer_and_amount(self):
-        self.accounts = sorted(self.accounts, key=lambda x: (x.owner.name, x.balance))
-        
+        self._accounts = sorted(self._accounts, key=lambda x: (x.owner.name, x.balance))
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def accounts(self):
+        return self._accounts
+
+
 class Customer:
-    def __init__(self, name, address, phone_number):
-        self.name = name
-        self.address = address
-        self.phone_number = phone_number
+    def __init__(self, name, address, phone_number, age):
+        self._name = name
+        self._address = address
+        self._phone_number = phone_number
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def phone_number(self):
+        return self._phone_number
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        if age >= 18:
+            self._age = age
+        else:
+            raise ValueError('You must be 18 years old')
 
 class Account:
     def __init__(self, account_number, balance, owner):
-        self.account_number = account_number
-        self.balance = balance
-        self.owner = owner
+        self._account_number = account_number
+        self._balance = balance
+        self._owner = owner
+
+    @property
+    def account_number(self):
+        return self._account_number
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def owner(self):
+        return self._owner
 
     def deposit(self, amount):
-        self.balance += amount
+        self._balance += amount
 
     def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
+        if self._balance >= amount:
+            self._balance -= amount
         else:
             print("Insufficient funds")
 
-## ----- testing data -----
+# ----- testing data -----
 # Create bank
 bank = Bank("Danske Bank")
 bank2 = Bank("Nordea")
 bank3 = Bank("Nykredit")
 
 # Create customers
-customer1 = Customer("John Doe", "Bygade 5", "+45 4040201")
-customer2 = Customer("Jane Smith", "Strandgade 17", "+45 65129503")
+customer1 = Customer("John Doe", "Bygade 5", "+45 4040201", 20)
+customer2 = Customer("Jane Smith", "Strandgade 17", "+45 65129503", 47)
 
 # Create accounts
 account1 = Account("123456", 170899, customer1)
@@ -48,7 +92,6 @@ account3 = Account("785012", 12505, customer2)
 account4 = Account("121416", 9910, customer1)
 account5 = Account("143257", 10010, customer1)
 account6 = Account("435932", 12900, customer2)
-
 
 # Add accounts to the bank
 bank.add_account(account1)
